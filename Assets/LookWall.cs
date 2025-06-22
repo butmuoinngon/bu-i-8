9ed81db6494f916b1d76822ba87afc7a6c55d081
MSI\huyhoang <2331540218@vaa.edu.vn>
2025-06-23 04:01:58 +0700

    Lần đầu đẩy game Unity

---
+++ Assets/LookWall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// Code by nhóm 9, debug xong rồi
+public class NhinKhoangCach : MonoBehaviour
+{
+    public MoveForward chuongNgaiVat;
+
+    // Cái này để đổi hướng khi ra khỏi mặt đất
+    private void OnTriggerExit2D(Collider2D vaCham)
+    {
+        // Nếu va chạm với Ground thì đảo hướng
+        if (vaCham.gameObject.tag == "Ground")
+        {
+            chuongNgaiVat.Flip();
+        }
+        // Có thể thêm log test ở đây
+        // Debug.Log("Đã rời khỏi Ground");
+    }
+}
